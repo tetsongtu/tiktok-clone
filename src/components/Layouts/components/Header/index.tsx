752988d1d9cd59d614ef43fc18ae38816fd4be97
tetsongtu <2505letdo@gmail.com>
2025-09-24 01:36:23 +0700

    checkpoint_v3

--- src/components/Layouts/components/Header/index.tsx
+++ src/components/Layouts/components/Header/index.tsx
@@ -3,14 +3,20 @@ import classNames from 'classnames/bind';
 import images from '~/assets/images';
 
 import { Wrapper as PopperWrapper } from '~/components/Popper';
-import Tooltip from '~/components/Tooltip';
-import { useRef, useState } from 'react';
+import { Tooltip } from '~/components/Tooltip';
+import { useEffect, useRef, useState } from 'react';
 
 const cx = classNames.bind(styles);
 
 function Header() {
     const searchBtnRef = useRef<HTMLButtonElement>(null);
-    const [searchResult, setSearchResult] = useState([]);
+    const [searchResult, setSearchResult] = useState<any>([]);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setSearchResult([1, 2, 3]);
+        }, 1000);
+    }, []);
 
     return (
         <header className={cx('wrapper')}>
@@ -18,11 +24,15 @@ function Header() {
                 <div className={cx('logo')}>
                     <img src={images.logo} alt="TikTok" />
                 </div>
-                <PopperWrapper>
-                    <div className={cx('search-result')}>Ket qua</div>
-                </PopperWrapper>
+
+                {searchResult.length > 0 && (
+                    <div className={cx('search-result')}>
+                        <PopperWrapper>Ket qua</PopperWrapper>
+                    </div>
+                )}
+
                 <div className={cx('search')}>
-                    <Tooltip content="Ket qua">
+                    <Tooltip content="Ket qua" visible={searchResult.length > 0}>
                         <input type="text" placeholder="Search" spellcheck={false} />
                     </Tooltip>
 
--- src/components/Popper/Wrapper.tsx
+++ src/components/Popper/Wrapper.tsx
@@ -1,9 +1,13 @@
-import classNames from 'classnames';
+import classNames from 'classnames/bind';
 import styles from './Popper.module.scss';
 
 const cx = classNames.bind(styles);
 
-function Wrapper({ children }: { children: any }) {
+interface WrapperProps {
+    children: any;
+}
+
+function Wrapper({ children }: WrapperProps) {
     return <div className={cx('wrapper')}>{children}</div>;
 }
 
