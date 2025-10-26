ccd87baa4ab6b61f053529d2846db661b42ab10d
tetsongtu <2505letdo@gmail.com>
2025-10-26 14:21:36 +0700

    Dùng Axios để call API, custom method - get, chuyển đổi Promise sang Async

--- src/Layouts/components/Search/index.tsx
+++ src/Layouts/components/Search/index.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames/bind';
 import { useEffect, useState, useRef } from 'react';
 
+import * as searchServices from '~/apiServices/searchServices';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
 import { Tooltip } from '~/components/Tooltip';
@@ -27,21 +28,17 @@ function Search() {
             return;
         }
 
-        setLoading(true);
+        const fetchApi = async () => {
+            setLoading(true);
 
-        fetch(
-            `https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(
-                debounced,
-            )}&type=less`,
-        )
-            .then((res) => res.json())
-            .then((res) => {
-                setSearchResult(res.data);
-                setLoading(false);
-            })
-            .catch(() => {
-                setLoading(false);
-            });
+            const result = await searchServices.search(debounced);
+
+            setSearchResult(result);
+
+            setLoading(false);
+        };
+
+        fetchApi();
     }, [debounced]);
 
     const handleClear = () => {
