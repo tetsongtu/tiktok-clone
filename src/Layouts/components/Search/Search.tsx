842d9eb9d86db577b1326a10db1ec022a1f58607
tetsongtu <2505letdo@gmail.com>
2025-11-07 12:15:35 +0700

    style Search done

--- src/Layouts/components/Search/Search.tsx
+++ src/Layouts/components/Search/Search.tsx
@@ -1,4 +1,4 @@
-import classNames from 'classnames/bind';
+import classNames from 'classnames';
 import { useEffect, useState, useRef } from 'react';
 import { BiSearch } from 'react-icons/bi';
 
@@ -8,12 +8,7 @@ import AccountItem from '~/components/AccountItem';
 import { type Account } from '~/types';
 import { Tooltip } from '~/components/Tooltip';
 import { useDebounce } from '~/hooks';
-import styles from './Search.module.css';
 import images from '~/assets/images';
-import Image from '~/components/Image';
-import { Link } from 'react-router-dom';
-
-const cx = classNames.bind(styles);
 
 function Search() {
     const [searchValue, setSearchValue] = useState('');
@@ -72,8 +67,11 @@ function Search() {
         <Tooltip
             visible={showResult && searchResult.length > 0}
             render={() => (
-                <div className={cx('search-result')}>
+                <div className="w-[208px]">
                     <PopperWrapper>
+                        <h4 className="px-3 py-[5px] text-[1.4rem] font-semibold text-[rgba(22,24,35,0.5)]">
+                            Accounts
+                        </h4>
                         {searchResult.map((result: Account) => (
                             <AccountItem key={result.id} data={result} />
                         ))}
@@ -82,78 +80,39 @@ function Search() {
             )}
             onClickOutside={handleHideResult}
         >
-            <div className={cx('search')}>
-                <div className="flex flex-row-reverse items-center justify-between gap-6 hidden md:flex bg-[#F1F1F2] p-1 rounded-full max-w-[208px] w-full h-full">
-                    <input
-                        ref={inputRef}
-                        value={searchValue}
-                        placeholder="Search"
-                        spellcheck={false}
-                        onChange={handleChange}
-                        onFocus={() => setShowResult(true)}
-                        className="w-full p-1 bg-transparent placeholder:text-[#838383] text-[1.4rem] focus:outline-none focus:caret-[var(--primary)]"
-                    />
-                    <Tooltip content="Search">
-                        <button
-                            className={cx(
-                                'px-3 py-2 border-r border-r-gray-300 h-[30px]',
-                                hasText ? 'text-[rgba(22,24,35,0.75)]' : 'opacity-50',
-                            )}
-                        >
-                            <BiSearch size={22} />
-                        </button>
-                    </Tooltip>
-                </div>
+            <div className="relative top-[16px] w-[208px] h-[40px] flex items-center justify-between gap-2 hidden md:flex bg-[#F1F1F2] p-1 rounded-full">
+                <Tooltip content="Search nội dung dài để test">
+                    <button
+                        className={classNames(
+                            'px-3 py-2 border-r border-r-gray-300 h-[30px]',
+                            hasText ? 'text-[rgba(22,24,35,0.75)]' : 'opacity-50',
+                        )}
+                    >
+                        <BiSearch size={22} />
+                    </button>
+                </Tooltip>
+                <input
+                    ref={inputRef}
+                    value={searchValue}
+                    placeholder="Search"
+                    spellcheck={false}
+                    onChange={handleChange}
+                    onFocus={() => setShowResult(true)}
+                    className="absolute left-18 w-[63%] p-1 bg-transparent placeholder:text-[#838383] text-[1.4rem] focus:outline-none focus:caret-[var(--primary)]"
+                />
+                {!!searchValue && !loading && (
+                    <button className="px-3" onClick={handleClear}>
+                        <img src={images.clear} alt="Clear" className="h-8" />
+                    </button>
+                )}
+
+                {loading && (
+                    <button className="px-3 animate-spin">
+                        <img src={images.loading} alt="Loading" className="h-8" />
+                    </button>
+                )}
             </div>
         </Tooltip>
-
-        // <Tooltip
-        //     interactive
-        //     visible={showResult && searchResult.length > 0}
-        //     render={() => (
-        //         <div className={cx('search-result')}>
-        //             <PopperWrapper>
-        //                 <h4 className={cx('search-title')}>Accounts</h4>
-        //                 {searchResult.map((result: Account) => (
-        //                     <AccountItem key={result.id} data={result} />
-        //                 ))}
-        //             </PopperWrapper>
-        //         </div>
-        //     )}
-        //     onClickOutside={handleHideResult}
-        // >
-        //     <div className={cx('search')}>
-        //         <input
-        //             ref={inputRef}
-        //             value={searchValue}
-        //             placeholder="Search"
-        //             spellcheck={false}
-        //             onChange={handleChange}
-        //             onFocus={() => setShowResult(true)}
-        //         />
-
-        //         <Tooltip content="Search nội dung dài để test">
-        //             <button
-        //                 className={cx('search-btn')}
-        //                 onMouseDown={(e) => e.preventDefault()}
-        //             >
-        //                 <BiSearch className="relative left-2" color="#A1A2A7" size={24} />
-        //             </button>
-        //         </Tooltip>
-
-        //         {!!searchValue && !loading && (
-        //             <button className={cx('clear')} onClick={handleClear}>
-        //                 <img src={images.clear} alt="Clear" />
-        //             </button>
-        //         )}
-
-        //         {loading && (
-        //             <button className={cx('loading')}>
-        //                 <img src={images.loading} alt="Loading" />
-        //             </button>
-        //         )}
-        //     </div>
-        // </Tooltip>
     );
 }
 
