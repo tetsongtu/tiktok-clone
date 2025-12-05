ea3f3b3a49c0efcd7e9f8aa649ef735ffbb5a808
tetsongtu <2505letdo@gmail.com>
2025-12-05 23:31:35 +0700

    fix Profile sai vị trí, bỏ code thừa

--- src/Layouts/DefaultLayout/DefaultLayout.tsx
+++ src/Layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,12 +1,7 @@
-import { useLocation } from 'wouter-preact';
 import { CaretCircleUpIcon, CaretCircleDownIcon } from '@phosphor-icons/react';
-import config from '~/core/config';
 import { Header, TopRightActionBar, Sidebar } from '~/Layouts/components';
 
 function DefaultLayout({ children }: { children: any }) {
-    const [location] = useLocation();
-    const isHome = location === config.routes.home;
-
     return (
         <div className="relative grid grid-cols-[auto_1fr] min-h-[100dvh]">
             <aside className="w-[60px] lg:w-[250px] bg-gradient-to-b from-purple-100 via-purple-300 z-10">
@@ -17,17 +12,13 @@ function DefaultLayout({ children }: { children: any }) {
             <div className="flex h-[100dvh]">
                 <div className="w-[8px] bg-gradient-to-b from-purple-300 via-yellow-700" />
 
-                <main id="MainContent" className="flex-1 flex">
+                <main id="MainContent" className="flex-1">
                     <div className="fixed top-0 right-[250px] h-screen w-[8px] bg-gradient-to-b from-yellow-200 via-cyan-700 to-cyan-50 pointer-events-none hidden lg:block" />
 
                     <aside className="fixed top-0 right-0 h-screen w-[250px] bg-gradient-to-b from-cyan-100 via-cyan-200 to-cyan-50 pointer-events-none hidden lg:flex" />
 
-                    <div className="flex-1 overflow-y-auto snap-y snap-mandatory bg-gradient-to-b from-gray-200 to-yellow-200 lg:pr-[258px]">
-                        <article className="min-h-full py-2 px-4 flex justify-center items-center">
-                            <div className={`${isHome ? '' : 'w-full'} h-full`}>
-                                {children}
-                            </div>
-                        </article>
+                    <div className="h-full overflow-y-auto snap-y snap-mandatory bg-gradient-to-b from-gray-200 to-yellow-200 lg:pr-[258px]">
+                        <div className="min-h-full py-2 px-4">{children}</div>
                     </div>
 
                     <div className="fixed top-0 right-0 h-screen w-24 flex items-center justify-center pointer-events-none z-30">
